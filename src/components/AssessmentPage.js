039b848d3c26929f53c60b729cb9e158653441c5
cbussalleu <100375866+cbussalleu@users.noreply.github.com>
2025-02-03 05:24:04 +0100

    Update AssessmentPage.js

--- src/components/AssessmentPage.js
+++ src/components/AssessmentPage.js
@@ -1,5 +1,7 @@
+'use client';
+
 import React from 'react';
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { ArrowRight } from 'lucide-react';
 
