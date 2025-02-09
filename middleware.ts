e41d315de16df843dfa234b063252c9020d300f5
cbussalleu <100375866+cbussalleu@users.noreply.github.com>
2025-02-09 22:33:44 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,14 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
+export function middleware(request: NextRequest) {
+  // Permitir acceso al endpoint del webhook
+  if (request.nextUrl.pathname.startsWith('/api/typeform-webhook')) {
+    return NextResponse.next()
+  }
+}
+
+// Configurar las rutas que no requieren autenticación
+export const config = {
+  matcher: '/api/:path*',
+}
