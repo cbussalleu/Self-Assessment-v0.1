14d603a7b21d1eb4a64f3ee1d7105ac646a2d944
cbussalleu <100375866+cbussalleu@users.noreply.github.com>
2025-02-23 22:44:42 +0100

    Update page.js

--- src/app/assessment/page.js
+++ src/app/assessment/page.js
@@ -11,9 +11,16 @@ export default function AssessmentForm() {
           id="MB0YptnS"
           style={{ width: '100%', height: '100%' }} 
           className="my-form"
-          onSubmit={() => {
-            // Redirigir a la página de resultados
-            window.location.href = '/results';
+          onSubmit={(event) => {
+            // Obtener el responseId del evento de submit
+            const responseId = event.responseId; // Asegúrate de que el evento contiene el responseId
+            
+            // Redirigir a la página de resultados con el responseId
+            if (responseId) {
+              window.location.href = `/results?responseId=${responseId}`;
+            } else {
+              window.location.href = '/results';
+            }
           }}
         />
       </div>
